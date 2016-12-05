07dc6e1fc5d9c4fa83117bd0556273acfbf85cca
Illedran <nrd.nardelli@gmail.com>
2016-12-05 11:57:27 +0100

    Added new data collection

--- scripts/atlas-higgs_2.py
+++ scripts/atlas-higgs_2.py
@@ -5,44 +5,46 @@ from keras.models import Sequential
 from keras.layers import Dense, BatchNormalization
 from keras.layers.advanced_activations import ELU
 from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.metrics import roc_auc_score, f1_score, precision_score, recall_score, accuracy_score, matthews_corrcoef
-import matplotlib.pyplot as plt
-from utils import parse_dataset, get_train_test_data
+from utils import parse_dataset, get_train_test_data, save_stats
 
 np.set_printoptions(suppress=True)
-dataset_num = 1
-
-X, y = parse_dataset(dataset_num)
-rows, cols = X.shape
 
 
-starting_size = 50
-encoded_dim = 5
-layers = 5
-nb_epoch = 5000
+configuration = {
+    'dataset_num' : 1,
+    'starting_dim': 50,
+    'encoded_dim': 5,
+    'layers_num': 5,
+    'nb_epoch': 1,
+    'batch_normalization': True
+}
+
 activation = ELU
-batch_normalization = True
-step = (starting_size - encoded_dim) // layers
+step = (configuration['starting_dim'] - configuration['encoded_dim']) // configuration['encoded_dim']
+
 def autoencoder():
     autoencoder = Sequential()
-    autoencoder.add(Dense(starting_size, input_dim=cols))
+    autoencoder.add(Dense(configuration['starting_dim'], input_dim=cols))
     autoencoder.add(activation())
-    if batch_normalization:
+    if configuration['batch_normalization']:
         autoencoder.add(BatchNormalization())
-    for i in range(starting_size-step, encoded_dim, -step):
+    for i in range(configuration['starting_dim'] - step, configuration['encoded_dim'], -step):
         autoencoder.add(Dense(i))
         autoencoder.add(activation())
-        if batch_normalization:
+        if configuration['batch_normalization']:
             autoencoder.add(BatchNormalization())
-    for i in range(encoded_dim, starting_size+step, step):
+    for i in range(configuration['encoded_dim'], configuration['starting_dim'] + step, step):
         autoencoder.add(Dense(i))
         autoencoder.add(activation())
-        if batch_normalization:
+        if configuration['batch_normalization']:
             autoencoder.add(BatchNormalization())
     autoencoder.add(Dense(cols))
     autoencoder.compile(optimizer='adadelta', loss='mse')
     autoencoder.summary()
     return autoencoder
+
+X, y = parse_dataset(configuration['dataset_num'])
+rows, cols = X.shape
 
 X_train, y_train, X_test, y_test = get_train_test_data(X, y)
 
@@ -55,43 +57,12 @@ X_train = preproc.transform(X_train)
 X_test = preproc.transform(X_test)
 
 # Preparing model
-autoencoder = KerasRegressor(build_fn=autoencoder, nb_epoch=nb_epoch, shuffle=True, batch_size=rows//100)
+autoencoder = KerasRegressor(build_fn=autoencoder, nb_epoch=configuration['nb_epoch'], shuffle=True, batch_size=rows // 100)
 
 autoencoder.fit(X_train, X_train)
 X_predict = autoencoder.predict(X_test)
 
 difference = np.linalg.norm(X_predict - X_test, axis=1)
 
-thresholds = 100
-thresh = []
-rocauc = []
-f1 = []
-prec = []
-rec = []
-acc = []
-corr = []
-# np.linspace(difference.min(), difference.max(), num=thresholds)
-# np.linspace(difference[y_test==0.].mean()-difference[y_test==0.].var()*2, difference[y_test==1.].mean()+difference[y_test==1.].var()*2, num=thresholds)
-for n in np.linspace(difference[y_test==0.].mean()-difference[y_test==0.].var()*2, difference[y_test==1.].mean()+difference[y_test==1.].var()*2, num=thresholds):
-    y_predict = (difference >= n).astype(np.bool)
-    y_test = y_test.astype(np.bool)
-    thresh.append(n)
-    rocauc.append(roc_auc_score(y_test, y_predict))
-    f1.append(f1_score(y_test, y_predict))
-    prec.append(precision_score(y_test, y_predict))
-    rec.append(recall_score(y_test, y_predict))
-    acc.append(accuracy_score(y_test, y_predict))
-    corr.append(matthews_corrcoef(y_test, y_predict))
-    anom_n = y_predict.sum()
-    #print(n, anom_n, len(y_predict) - anom_n)
-
-plt.plot(thresh, rocauc, label="ROC AUC")
-plt.plot(thresh, f1, label="F1")
-plt.plot(thresh, prec, label="Precision")
-plt.plot(thresh, rec, label="Recall")
-plt.plot(thresh, acc, label="Accuracy")
-plt.plot(thresh, corr, label="Corr")
-plt.legend()
-plt.xlim([difference[y_test==0.].mean()-difference[y_test==0.].var()*2, difference[y_test==1.].mean()+difference[y_test==1.].var()*2])
-plt.ylim([0,1])
-plt.savefig("../results/{0}-{1}-{2}-{3}-{4}-{5}.png".format(dataset_num, starting_size, encoded_dim, layers, nb_epoch, 'bn' if batch_normalization else 'no'))
+print("Saving stats...")
+save_stats(configuration, difference, y_test)
--- scripts/utils.py
+++ scripts/utils.py
@@ -2,6 +2,12 @@ import numpy as np
 import pandas as pd
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_auc_score, f1_score, precision_score, recall_score, accuracy_score, matthews_corrcoef
+import os
 
 
 def parse_dataset(nan_class=None):
@@ -12,7 +18,8 @@ def parse_dataset(nan_class=None):
             classes = f.read().split(',')
             datasets = {}
             for cls in classes:
-                atlas_data = pd.read_hdf("../data/atlas-higgs_{}.hdf".format(cls), "atlas_data").values.astype(np.float32)
+                atlas_data = pd.read_hdf("../data/atlas-higgs_{}.hdf".format(cls), "atlas_data").values.astype(
+                    np.float32)
                 X = atlas_data[:, :-1]
                 y = atlas_data[:, -1]
                 datasets[cls] = (X, y)
@@ -24,6 +31,7 @@ def parse_dataset(nan_class=None):
         return X, y
     else:
         return None
+
 
 def get_train_test_data(X, y, random_state=1337):
     X_b = X[y == 0.]  # Non-anomaly
@@ -38,3 +46,100 @@ def get_train_test_data(X, y, random_state=1337):
     X_test, y_test = shuffle(np.vstack([X_b_test, X_s]), np.hstack([y_b_test, y_s]), random_state=random_state)
 
     return (X_train, y_train, X_test, y_test)
+
+
+def save_stats(configuration, difference, y_test):
+    thresholds = 250
+    metrics = {
+        'thresh': {
+            'data': [],
+            'label': 'Threshold',
+            'plotted': False
+        },
+        'rocauc': {
+            'data': [],
+            'label': 'ROC AUC',
+            'plotted': True
+        },
+        'f1': {
+            'data': [],
+            'label': 'F1',
+            'plotted': True
+        },
+        'prec': {
+            'data': [],
+            'label': 'Precision',
+            'plotted': True
+        },
+        'rec': {
+            'data': [],
+            'label': 'Recall',
+            'plotted': True
+        },
+        'acc': {
+            'data': [],
+            'label': 'Accuracy',
+            'plotted': True
+        },
+        'corr': {
+            'data': [],
+            'label': 'Correlation',
+            'plotted': True
+        },
+        'num_anomalies': {
+            'data': [],
+            'label': 'Number of anomalies',
+            'plotted': False
+        },
+        'num_regular': {
+            'data': [],
+            'label': 'Number of regular data',
+            'plotted': False
+        }
+
+    }
+    # np.linspace(difference.min(), difference.max(), num=thresholds)
+    # np.linspace(difference[y_test==0.].mean()-difference[y_test==0.].var()*2, difference[y_test==1.].mean()+difference[y_test==1.].var()*2, num=thresholds)
+    min_x = max(0, difference[y_test == 0.].mean() - difference[y_test == 0.].var() * 5)
+    max_x = difference[y_test == 1.].mean() + difference[y_test == 1.].var() * 5
+    for n in np.linspace(min_x, max_x, num=thresholds):
+        y_predict = (difference >= n).astype(np.bool)
+        y_test = y_test.astype(np.bool)
+        metrics['thresh']['data'].append(n)
+        metrics['rocauc']['data'].append(roc_auc_score(y_test, y_predict))
+        metrics['f1']['data'].append(f1_score(y_test, y_predict))
+        metrics['prec']['data'].append(precision_score(y_test, y_predict))
+        metrics['rec']['data'].append(recall_score(y_test, y_predict))
+        metrics['acc']['data'].append(accuracy_score(y_test, y_predict))
+        metrics['corr']['data'].append(matthews_corrcoef(y_test, y_predict))
+        metrics['num_anomalies']['data'].append(y_predict.sum())
+        metrics['num_regular']['data'].append(len(y_predict) - y_predict.sum())
+
+    for metric in metrics:
+        if metrics[metric]['plotted']:
+            plt.plot(metrics['thresh']['data'], metrics[metric]['data'], label=metrics[metric]['label'])
+    plt.legend()
+    plt.xlim([min_x, max_x])
+    plt.ylim([0, 1])
+
+    out_file = "{0}-{1}-{2}-{3}-{4}-{5}".format(configuration['dataset_num'],
+                                                configuration['starting_dim'],
+                                                configuration['encoded_dim'],
+                                                configuration['layers_num'],
+                                                configuration['nb_epoch'],
+                                                'bn' if configuration['batch_normalization'] else 'no')
+    if not os.path.exists("../results/" + out_file):
+        os.mkdir("../results/" + out_file)
+    plt.savefig("../results/" + out_file + "/metrics_plots.png")
+
+    with open("../results/" + out_file + "/metadata.txt", 'w') as f:
+        f.write("Test set data (anomalies, non_anomalies): %d, %d\n" % (
+            len(y_test[y_test == 1.]), len(y_test[y_test == 0.])))
+        f.write("Reconstruction error mean (anomalies, non_anomalies): %d, %d\n" % (
+            difference[y_test == 0.].mean(), difference[y_test == 1.].mean()))
+        f.write("Reconstruction error var (anomalies, non_anomalies): %d, %d\n" % (
+            difference[y_test == 0.].var(), difference[y_test == 1.].var()))
+
+    pd.DataFrame(data=np.stack([metrics[metric]['data'] for metric in metrics]).T,
+                 columns=[metrics[metric]['label'] for metric in metrics]).to_hdf(
+        "../results/" + out_file + "/metrics.hdf", "metrics", mode='w', complib='zlib', complevel=9)
