d348acea779665d27810ad35f7157db62d469e19
Illedran <nrd.nardelli@gmail.com>
2017-01-05 19:04:54 +0100

    Added autoencoder class

--- scripts/autoencoder.py
+++ scripts/autoencoder.py
@@ -1,41 +1,48 @@
-from keras.models import Model, Sequential
+from keras.models import Model
 from keras.layers import Dense, BatchNormalization, Activation, Input
 
-def create_dense_autoencoder(input_dimensions, latent_dimensions, layers, activation='sigmoid',
-                             optimizer='adadelta', loss='mse', batch_normalization=False):
-    input_layer = Input(shape=(input_dimensions,))
-    x = input_layer
-    for layer_dims in layers:
-        x = Dense(layer_dims)(x)
-        if activation is not None:
+class Autoencoder(Model):
+
+    def __init__(self, input_dimensions, latent_dimensions, layers, activation='sigmoid',
+                    optimizer='adadelta', loss='mse', batch_normalization=False):
+        input_layer = Input(shape=(input_dimensions,))
+        x = input_layer
+        for layer_dims in layers:
+            x = Dense(layer_dims)(x)
+            if activation is not None:
+                if type(activation) is str:
+                    x = Activation(activation)(x)
+                else:
+                    x = Activation(activation())(x)
+            if batch_normalization:
+                x = BatchNormalization()(x)
+        encoded = Dense(latent_dimensions)(x)
+        x = encoded
+        for layer_dims in reversed(layers):
+            x = Dense(layer_dims)(x)
             if type(activation) is str:
                 x = Activation(activation)(x)
             else:
                 x = Activation(activation())(x)
-        if batch_normalization:
-            x = BatchNormalization()(x)
-    encoded = Dense(latent_dimensions)(x)
-    x = encoded
-    for layer_dims in reversed(layers):
-        x = Dense(layer_dims)(x)
-        if type(activation) is str:
-            x = Activation(activation)(x)
-        else:
-            x = Activation(activation())(x)
-        if batch_normalization:
-            x = BatchNormalization()(x)
-    decoded = Dense(input_dimensions)(x)
+            if batch_normalization:
+                x = BatchNormalization()(x)
+        decoded = Dense(input_dimensions)(x)
 
-    autoencoder = Model(input=input_layer, output=decoded)
+        super().__init__(input=input_layer, output=decoded)
 
-    encoder = Model(input=input_layer, output=encoded)
+        # Encoder sub-model
+        self.encoder = Model(input=input_layer, output=encoded)
 
-    encoded_input = Input(shape=(latent_dimensions,))
-    x = encoded_input
-    for decoding_layer in autoencoder.layers[len(autoencoder.layers)//2+1:]:
-        x = decoding_layer(x)
-    decoder = Model(input=encoded_input, output=x)
+        # Decoder sub-model
+        encoded_input = Input(shape=(latent_dimensions,))
+        x = encoded_input
+        for decoding_layer in self.layers[len(self.layers) // 2 + 1:]:
+            x = decoding_layer(x)
 
-    autoencoder.compile(optimizer=optimizer, loss=loss)
+        self.decoder = Model(input=encoded_input, output=x)
 
-    return autoencoder, encoder, decoder
+        self.compile(optimizer=optimizer, loss=loss)
+
+        self.activation = activation
+        self.batch_normalization = batch_normalization
+
