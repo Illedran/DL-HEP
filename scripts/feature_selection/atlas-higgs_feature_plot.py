2c6406762f146691967bee5db543ca462dc96234
Illedran <nrd.nardelli@gmail.com>
2017-01-17 11:09:31 +0100

    Added loss plots and feature plots

--- scripts/feature_selection/atlas-higgs_feature_plot.py
+++ scripts/feature_selection/atlas-higgs_feature_plot.py
@@ -1,13 +1,5 @@
-import numpy as np
 import pandas as pd
-from sklearn.preprocessing import RobustScaler, StandardScaler, MinMaxScaler
-from sklearn.preprocessing import Imputer
-from sklearn.pipeline import Pipeline
-from scripts.utils import parse_dataset
-from scripts.autoencoder import create_dense_autoencoder
-from keras.layers.advanced_activations import ELU
 import os
-from tqdm import tqdm
 
 # No import errors
 if 'DISPLAY' in os.environ:
@@ -35,8 +27,8 @@ atlas_higgs = atlas_higgs.drop(['EventId', 'KaggleWeight', 'Weight', 'KaggleSet'
 for idx, col in enumerate(atlas_higgs.columns):
     print(col)
     ax = plt.subplot(5, 6, idx + 1)
-    plt.hist(atlas_higgs[col][labels == 1].dropna(), color='green', alpha=0.6, normed=True)
-    plt.hist(atlas_higgs[col][labels == 0].dropna(), color='red', alpha=0.6, normed=True)
+    plt.hist(atlas_higgs[col][labels == 0].dropna(), bins=20, color='#D55E00', alpha=0.6, normed=True)
+    plt.hist(atlas_higgs[col][labels == 1].dropna(), bins=20, color='#0072B2', alpha=0.6, normed=True)
     ax.set_title(col)
 
 plt.subplots_adjust(wspace=0.4, hspace=0.4)
