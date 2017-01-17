4cd9653691a33f238831d44b4a889825baa06fd9
Illedran <nrd.nardelli@gmail.com>
2017-01-17 11:46:29 +0100

    Updated autoencoder...

--- scripts/autoencoder.py
+++ scripts/autoencoder.py
@@ -1,10 +1,15 @@
 from keras.models import Model
 from keras.layers import Dense, BatchNormalization, Activation, Input
+import numpy as np
+
+# Automatically split into 4 subclasses based on PRI_jet_num
+# [0, 1, 2, 3]
+PRI_jet_num_classes = [0, 1, 2, 3]
+
 
 class Autoencoder(Model):
-
     def __init__(self, input_dimensions, latent_dimensions, layers, activation='sigmoid',
-                    optimizer='adadelta', loss='mse', batch_normalization=False):
+                 batch_normalization=False, **kwargs):
         input_layer = Input(shape=(input_dimensions,))
         x = input_layer
         for layer_dims in layers:
@@ -41,8 +46,28 @@ class Autoencoder(Model):
 
         self.decoder = Model(input=encoded_input, output=x)
 
-        self.compile(optimizer=optimizer, loss=loss)
-
         self.activation = activation
         self.batch_normalization = batch_normalization
 
+
+# class BaggingAutoencoder():
+#     def __init__(self, train_data, latent_dimensions, layers, **kwargs):
+#         self.column_mask = {jet_class: np.logical_not(
+#             train_data[train_data.PRI_jet_num == jet_class].isnull().sum() ==
+#             len(train_data[train_data.PRI_jet_num == jet_class])
+#         ) for jet_class in [0, 1, 2, 3]}
+#
+#         self.classes = {}
+#         for jet_class in PRI_jet_num_classes:
+#             self.column_mask[jet_class].PRI_jet_num = False
+#             self.classes[jet_class] = Autoencoder(input_dimensions=self.column_mask[jet_class].sum(),
+#                                                        latent_dimensions=latent_dimensions,
+#                                                        layers=layers, **kwargs)
+#
+#     def fit(self, x, **kwargs):
+#         for jet_class in PRI_jet_num_classes:
+#             self.classes[jet_class].fit(x[x.PRI_jet_num == jet_class, self.column_mask[jet_class]], **kwargs)
+#
+#     def predict(self, x, **kwargs):
+#         for jet_class in PRI_jet_num_classes:
+#             self.classes[jet_class].predict(x[x.PRI_jet_num == jet_class, self.column_mask[jet_class]], **kwargs)
